eb7ddc8f1432b13926749e6162e50f11a2ee0b44
Logan Esch <lesch@live.com>
2014-01-21 10:52:26 -0700

    Sound tower code should be complete, needs tested though

--- teams/team130/SoundTower.java
+++ teams/team130/SoundTower.java
@@ -1,6 +1,7 @@
 package team130;
 
 import battlecode.common.Direction;
+import battlecode.common.GameActionException;
 import battlecode.common.MapLocation;
 
 public class SoundTower extends RobotPlayer {
@@ -8,14 +9,29 @@ public class SoundTower extends RobotPlayer {
 	 * Array of distances to shoot the noise. <br>
 	 * <br>
 	 * First shot should be "Small." <br>
-	 * Rest should be "Large."<br>
+	 * Rest should be "Large."
 	 */
 	private static final int[] RANGE = { 20, 17, 11, 5 };
+
+	/**
+	 * Variable to track the index of the range last shot.
+	 */
 	private static int lastRangeShotIndex = 0;
+
+	/**
+	 * Variable to track direction last shot.
+	 */
 	private static Direction directionLastShot = Direction.NORTH_WEST;
 
 	public static void run() {
 		try {
+			if(rc.isActive()){
+				shootNoise();
+				//reset this variable. 
+				if(lastRangeShotIndex == RANGE.length){
+					lastRangeShotIndex = 0;
+				}
+			}
 
 		} catch (Exception e) {
 			System.out.println("Sound Tower Exception");
@@ -24,26 +40,39 @@ public class SoundTower extends RobotPlayer {
 
 	}
 
-	private static void shootNoise() {
+	private static void shootNoise() throws GameActionException {
 		/**
-		 * MapLocation to shoot next. 
+		 * MapLocation to shoot next.
 		 */
 		MapLocation target = null;
 
-		distanceLoop: for (int i = lastRangeShotIndex; i < RANGE.length; i++) {
-			if(i == 0){
+		distanceLoop: for (; lastRangeShotIndex < RANGE.length; lastRangeShotIndex++) {
+
+			// we've fired at all distances in this direction, change
+			// direction.
+			if (lastRangeShotIndex == 0) {
 				updateNextDirectionToShoot();
 			}
-			
-			target = myLoc.add(directionLastShot, RANGE[i]);
-			
+
+			// this is the location we want to try to shoot.
+			target = myLoc.add(directionLastShot, RANGE[lastRangeShotIndex]);
+
+			// we need to make sure the target locaiton is within bounds.
 			if (target.x < 0 || target.y < 0 || target.x > rc.getMapWidth()
 					|| target.y > rc.getMapHeight()) {
+				// target out of bounds, restart loop.
 				break distanceLoop;
 			} else {
+				// target in bounds. Fire.
+				if (lastRangeShotIndex == 0) {
+					rc.attackSquareLight(target);
+				} else {
+					rc.attackSquare(target);
+				}
 				
+				//after we have shot, yield. 
+				rc.yield();
 			}
-
 		}
 	}
 
